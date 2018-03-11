5927e3b586f942399a118988955da4992db649b3
Marqn <marqnpl@gmail.com>
2018-03-11 22:03:20 +0100

    add state to simple button

--- src/DoneButton.js
+++ src/DoneButton.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 export default class DoneButton extends React.Component {
-    status = 'btn btn-outline-success';
+    constructor(props) {
+        super(props);
+        this.state = {status: 'btn btn-secondary', text: 'Wykonaj'};
+    }
+
+    changeState() {
+        if (this.state.status == 'btn btn-secondary')
+            this.setState({status: 'btn btn-success', text:'Zrobione'});
+        else if (this.state.status == 'btn btn-success')
+            this.setState({status: 'btn btn-secondary', text:'Wykonaj'});
+
+        console.log(this.state);
+    }
 
     render() {
         return (
-            <button class="btn btn-outline-success">Wykonaj</button>
+            <button class={this.state.status} onClick={this.changeState.bind(this)}>{this.state.text}</button>
         )
     }
 }
